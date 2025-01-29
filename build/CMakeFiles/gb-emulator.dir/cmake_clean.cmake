3398d21a79c5f47dcfe0ca74475678509dfef20d
JoostLE-2468995 <joost.lehaen@student.uhasselt.be>
2025-01-29 20:24:05 +0100

    Initial commit

---
+++ build/CMakeFiles/gb-emulator.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gb-emulator.dir/main.c.o"
+  "CMakeFiles/gb-emulator.dir/main.c.o.d"
+  "gb-emulator"
+  "gb-emulator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gb-emulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
